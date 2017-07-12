6b7d886f46de4943f373496e39d64deba21d6248
Stu Blair <stu314159@gmail.com>
2017-07-12 10:21:55 -0500

    implemented BC functions for D3Q19

--- D3Q19Lattice.cpp
+++ D3Q19Lattice.cpp
@@ -53,22 +53,43 @@ D3Q19Lattice::~D3Q19Lattice()
 
 void D3Q19Lattice::set_inlet_bc_micro(LBM_DataHandler& f)
 {
-  //finish me...
+	int sp[9]={7,9,11,13,16,19,21,23,25};
+	int bbSp[9]={20,22,24,26,3,6,8,10,12};
+	int numBB = 9;
+	for(int s=0;s<numBB;s++)
+	{
+		f.f[sp[s]]+=f.f[bbSp[s]]-f.fEq[bbSp[s]];
+	}
 }
 
 void D3Q19Lattice::set_inlet_bc_macro(LBM_DataHandler& f)
 {
-	//finish me...
+	f.rho = (1./(1. - f.uz))*(2.*(f.f[3]+f.f[6]+f.f[8]+
+			f.f[10]+f.f[12]+f.f[20]+f.f[22]+f.f[24]+f.f[26])+
+			(f.f[0]+f.f[1]+f.f[2]+f.f[4]+f.f[5]+
+					f.f[14]+f.f[15]+f.f[17]+f.f[18]));
+
 }
 
 void D3Q19Lattice::set_outlet_bc_micro(LBM_DataHandler& f)
 {
-	//finish me...
+	int sp[9]={20,22,24,26,3,6,8,10,12};
+	int bbSp[9]={7,9,11,13,16,19,21,23,25};
+	int numBB = 9;
+	for(int s=0;s<numBB;s++)
+	{
+		f.f[sp[s]]+=f.f[bbSp[s]]-f.fEq[bbSp[s]];
+	}
 }
 
 void D3Q19Lattice::set_outlet_bc_macro(LBM_DataHandler& f)
 {
-	//finish me...
+	f.uz = -1. + (1./f.rho)*(2.*
+			(f.f[3]+f.f[6]+f.f[8]+f.f[10]+f.f[12]+f.f[20]+
+					f.f[22]+f.f[24]+f.f[26])+
+					(f.f[0]+f.f[1]+f.f[2]+f.f[4]+f.f[5]+
+							f.f[14]+f.f[15]+f.f[17]+f.f[18]));
+
 }
 
 
