521f970b584293c6424ea5478542cb4358466a1e
Stu Blair <stu314159@gmail.com>
2017-07-12 10:44:57 -0500

    fixed bug in outlet macroscopic bc for D3Q19

--- D3Q19Lattice.cpp
+++ D3Q19Lattice.cpp
@@ -86,8 +86,8 @@ void D3Q19Lattice::set_outlet_bc_micro(LBM_DataHandler& f)
 void D3Q19Lattice::set_outlet_bc_macro(LBM_DataHandler& f)
 {
 	// check this.
-	f.uz = -1. + (1./f.rho)*(2.*(f.f[6]+f.f[13]+f.f[14]+
-			f.f[17]+f.f[18])+(f.f[0]+f.f[1]+f.f[2]+
+	f.uz = -1. + (1./f.rho)*(2.*(f.f[5]+f.f[11]+f.f[12]+
+			f.f[15]+f.f[16])+(f.f[0]+f.f[1]+f.f[2]+
 					f.f[3]+f.f[4]+f.f[7]+f.f[8]+f.f[9]+f.f[10]));
 
 
