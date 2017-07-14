4c0ffa00d7a36701052ab25e081ec7a9f6af1d7b
Stu Blair <stu314159@gmail.com>
2017-07-14 17:58:56 +0000

    working version with OMP acceleration

--- test_script.sh
+++ test_script.sh
@@ -11,7 +11,7 @@
 python ./NFCpp_preprocess.py
 
 # input
-python ./genInput.py sphere.mat $1 200 10 0 20 10 5e-3 0 0
+python ./genInput.py sphere.mat $1 2000 10 0 200 10 5e-3 0 0
 
 # run the code
 export OMP_NUM_THREADS=$2
